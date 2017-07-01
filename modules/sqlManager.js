83b70df1b29a2496978641862ea4a0756338b528
jpfeiffer16 <jpfeiffer@bluemodus.com>
2017-06-30 21:39:19 -0600

    Remove pool if it errors

--- modules/sqlManager.js
+++ modules/sqlManager.js
@@ -22,6 +22,17 @@ module.exports = (function() {
     pool.connect((err) => {
       if (err) {
         console.error(err);
+        //Remove the item from the array.
+        // poolCollection.forEach((item, index) => {
+        for(let i = 0; i < poolCollection.length; i++) {
+          let item = poolCollection[i];
+          if (item.config == config) {
+            poolCollection.splice(i, 1);
+            break;
+          }
+        }
+        // });
+        
         return;
       }
       cb(pool);
