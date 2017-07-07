6532c54599e5a75f64d325d8517d18b06dff887f
jpfeiffer16 <jpfeiffer@bluemodus.com>
2017-07-06 22:53:54 -0600

    Updating build script

--- build.sh
+++ build.sh
@@ -8,9 +8,11 @@ rm build/* -r
 
 ./node_modules/.bin/electron-packager ./ --platform linux --arch x64 --icon images/icon.png --out build/
 ./node_modules/.bin/electron-packager ./ --platform darwin --arch x64 --icon images/icon.png --out build/
+./node_modules/.bin/electron-packager ./ --platform win32 --arch x64 --icon images/icon.png --out build/
 
 cd build
 
 tar -cvzf querier-linux-x64.tar.gz querier-linux-x64
 zip -r querier-linux-x64.zip querier-linux-x64/*
 zip -r querier-darwin-x64.zip querier-darwin-x64/*
+zip -r querier-win32-x64.zip querier-win32-x64/*
